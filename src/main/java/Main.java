b3a3eb242cbb5e78483fe2d28b0a1ce26343490c
Hjordten <andr314e@stud.kea.dk>
2022-09-12 10:10:33 +0200

    Tilføjet get og set metoder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Person p1 = new Person(1.74, 77, 60); //atribute er her det som er i parentes. Altså, hoejde.
 
+        System.out.println(p1.getHoejde() + " " + p1.getVaegt());
+        p1.setHoejde(2.00);
+        System.out.println(p1.getHoejde());
+        System.out.println("");
+        System.out.println(p1.beregnBMI());
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,9 +1,39 @@
 public class Person {
 
     private double hoejde;
+    private int vaegt;
+    private int alder;
 
-    public Person(double hoejde){
+    public Person(double hoejde, int vaegt, int alder) {
         this.hoejde = hoejde;
+        this.vaegt = vaegt;
+        this.alder = alder;
+
+    }
+
+
+    public double getHoejde() { //get metoder har ALDRIG parameter, fordi den skal give noget retur.
+        return hoejde; //man skriver det som den skal returnere
+    }
+
+    public int getVaegt(){ //get metoder har ALDRIG parameter, fordi den skal give noget retur.
+        return vaegt; //man skriver det som den skal returnere
+    }
+
+    public void setHoejde(double hoejde) { //set metode som tillader os at sætte hoejden på et objekt, som vi definere i main
+        this.hoejde = hoejde; //set metoden.
+    }
+
+    public void setVaegt(int vaegt) {
+        this.vaegt = vaegt;
+    }
+    public double beregnBMI(){
+        double bmi = vaegt / Math.pow(hoejde,2);
+        return bmi;
+
+    }
+
+    public void harFødselsdag(){
 
     }
 }
